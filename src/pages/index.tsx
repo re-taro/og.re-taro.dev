3415887755c3e4b0cb4ef5fbe916549d39d54b7c
Rintaro Itokawa <me@re-taro.dev>
2022-11-17 22:35:13 +0900

    feat: Create top page
    ref: #2

---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+const Home = () => (
+  <section className="text-neutral-800 mx-auto my-0 max-w-[700px] space-y-3">
+    <h1 className="text-4xl">This is OGP Server for Rintaro!</h1>
+    <ul className="bg-gray-100 p-7">
+      <li className="list-disc list-inside">
+        GitHub:
+        <a
+          href="https://github.com/re-taro-dev/ogp.re-taro.dev"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          https://github.com/re-taro-dev/ogp.re-taro.dev
+        </a>
+      </li>
+    </ul>
+    <h2 className="text-2xl">Sample</h2>
+    <p>https://ogp.re-taro.dev/api/ogp?title=ogp.re-taro.dev</p>
+    <Image
+      src={"https://ogp.re-taro.dev/api/ogp?title=ogp.re-taro.dev"}
+      alt="OGP"
+      width={1200}
+      height={630}
+    />
+  </section>
+);
+
+export default Home;
