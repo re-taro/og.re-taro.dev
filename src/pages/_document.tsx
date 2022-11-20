11e312072adbef0bce471b12bd7b4b9a5e47b5bd
Rintaro Itokawa <me@re-taro.dev>
2022-11-20 22:02:28 +0900

    feat: Add _document.tsx for SEO

---
+++ src/pages/_document.tsx
@@ -0,0 +1,62 @@
+import type { DocumentContext } from "next/document";
+import NextDocument, {
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import type { ReactElement } from "react";
+
+class Document extends NextDocument {
+  static override getInitialProps(
+    context: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    return NextDocument.getInitialProps(context);
+  }
+
+  override render(): ReactElement {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta charSet="utf8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <meta name="description" content="Rintaro's OGP server" lang="en" />
+          <meta name="name" content="ogp.re-taro.dev" />
+          <meta
+            name="image"
+            content="https://ogp.re-taro.dev/api/ogp?title=ogp.re-taro.dev"
+          />
+          <meta property="og:title" content="ogp.re-taro.dev" />
+          <meta property="og:description" content="Rintaro's OGP server" />
+          <meta property="og:type" content="webpage" />
+          <meta property="og:url" content="https://ogp.re-taro.dev/" />
+          <meta
+            property="og:image"
+            content="https://ogp.re-taro.dev/api/ogp?title=ogp.re-taro.dev"
+          />
+          <meta property="og:locale" content="ja_JP" />
+          <meta property="og:site_name" content="ogp.re-taro.dev" />
+          <meta name="twitter:title" content="ogp.re-taro.dev" />
+          <meta name="twitter:description" content="Rintaro's OGP server" />
+          <meta
+            name="twitter:image"
+            content="https://ogp.re-taro.dev/api/ogp?title=ogp.re-taro.dev"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@10969_rintaro" />
+          <meta name="twitter:creator" content="@10969_rintaro" />
+          {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+          <title>OGP server for Rintaro</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
