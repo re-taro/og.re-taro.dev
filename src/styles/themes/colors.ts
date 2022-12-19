34fd507918804a6fd4a7bcdbb74f72e10a394dd7
Rintaro Itokawa <me@re-taro.dev>
2022-12-20 01:57:07 +0900

    feat: Add custom theme for chakra

---
+++ src/styles/themes/colors.ts
@@ -0,0 +1,32 @@
+import type { DeepPartial, Theme } from "@chakra-ui/react";
+
+/** extend additional color here */
+const extendedColors: DeepPartial<
+  Record<string, Theme["colors"]["blackAlpha"]>
+> = {
+  night: {
+    500: "#4C566A",
+    600: "#434C5E",
+    700: "#3B4252",
+    800: "#2E3440",
+  },
+  snow: {
+    500: "#ECEFF4",
+    600: "#E5E9F0",
+    700: "#D8DEE9",
+  },
+  frost: {
+    500: "#8FBCBB",
+    600: "#88C0D0",
+    700: "#81A1C1",
+    800: "#5E81AC",
+  },
+};
+
+/** override chakra colors here */
+const overridenChakraColors: DeepPartial<Theme["colors"]> = {};
+
+export const colors = {
+  ...overridenChakraColors,
+  ...extendedColors,
+};
---
+++ src/styles/themes/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+import { colors } from "./colors";
+import { components } from "./components";
+
+const customTheme = extendTheme({
+  colors,
+  components,
+});
+
+export default customTheme;
