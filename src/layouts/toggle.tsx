56aa9c82448d714272b1dcfa0ec02cd6f7416eaf
Rintaro Itokawa <me@re-taro.dev>
2022-12-24 19:58:35 +0900

    feat: create layout

---
+++ src/layouts/toggle.tsx
@@ -0,0 +1,14 @@
+import { IconButton, useColorMode } from '@chakra-ui/react'
+import { Icon } from '@iconify/react'
+import type { FC } from 'react'
+
+export const ThemeToggle: FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <IconButton
+      aria-label="theme toggle"
+      icon={colorMode === 'light' ? <Icon icon="ri:moon-fill" /> : <Icon icon="ri:sun-fill" />}
+      onClick={toggleColorMode}
+    />
+  )
+}
